af81833edd18407e7f13d5a0b396f910fcda3229
FrontMD <web@mockup.digital>
2024-07-04 17:13:43 +0300

    main page in progress

---
+++ src/js/import/components.js
@@ -0,0 +1,6 @@
+
+@@include("../../blocks/modules/footer/footer.js")
+@@include("../../blocks/modules/header/header.js")
+
+document.addEventListener('DOMContentLoaded', () => {
+})
---
+++ src/js/import/main.js
@@ -0,0 +1,61 @@
+
+
+/*function num_word(value, words) {
+	value = Math.abs(value) % 100;
+	var num = value % 10;
+	if (value > 10 && value < 20) return words[2];
+	if (num > 1 && num < 5) return words[1];
+	if (num == 1) return words[0];
+	return words[2];
+}
+
+function myLockBody() {
+	$('html').addClass('with-fancybox');
+	$('body').addClass('hide-scrollbar');
+}
+function myUnlockBody() {
+	$('html').removeClass('with-fancybox');
+	$('body').removeClass('hide-scrollbar');
+}
+
+$.fn.isXScrollable = function () {
+	return this[0].scrollWidth > this[0].clientWidth;
+};
+
+$.fn.isYScrollable = function () {
+	return this[0].scrollHeight > this[0].clientHeight;
+};
+
+$.fn.isScrollable = function () {
+	return this[0].scrollWidth > this[0].clientWidth || this[0].scrollHeight > this[0].clientHeight;
+};
+
+//Инициализация фансибокса
+document.addEventListener('DOMContentLoaded', () => {
+    Fancybox.bind("[data-fancybox]", {
+        placeFocusBack: false,
+        mainClass: 'my-fancybox',
+        idle: false,
+        Carousel: {
+            transition: "crossfade",
+            Navigation: {
+                prevTpl: '<svg><use xlink:href=img/sprites/sprite.svg#card_arrow></use></svg>',
+                nextTpl: '<svg><use xlink:href=img/sprites/sprite.svg#card_arrow></use></svg>',
+              },
+        },
+        Thumbs: {
+            type: "classic",
+        },
+        Toolbar: {
+            enabled: true,
+            display: {
+                left: [],
+                middle: [],
+                right: [
+                  "close",
+                ],
+            },
+        }
+
+    });
+})*/
